ca398e91f586f1b82d0c78969847024275097593
çağrı çamyar <cagri-95@hotmail.com>
2024-05-14 19:17:23 +0300

    Add project files.

---
+++ Dictionary/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(1, "Çağrı");
+            myDictionary.Add(2, "Emre");
+            Console.WriteLine(myDictionary.Count);
+
+            }
+        }
+
+        class MyDictionary <Key, Value>
+        {
+            Key[] keyArray;
+            Value[] valueArray;
+
+            Key[] tempKeyArray;
+            Value[] tempValueArray;
+
+            public MyDictionary()
+            {
+            keyArray = new Key[0];
+            valueArray = new Value[0];
+            }
+
+            public void Add (Key key, Value value)
+            {
+            tempKeyArray = keyArray;
+            tempValueArray = valueArray;
+            keyArray = new Key[keyArray.Length + 1];
+            valueArray = new Value[valueArray.Length + 1];
+            for (int i = 0; i < tempKeyArray.Length; i++) 
+            {
+                tempKeyArray[i] = keyArray[i];
+                tempValueArray[i] = valueArray[i];
+            }
+
+            keyArray[keyArray.Length - 1] = key;
+            valueArray[valueArray.Length - 1] =value;
+        
+            }
+
+        private int _count;
+
+        public int Count
+        {
+            get { return keyArray.Length; } 
+        }
+
+        }
+
+
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+            list.Add("a");
+            list.Add("b");
+            list.Add("c");
+            Console.WriteLine(list.Count);
+
+            MyList<string> myList = new MyList<string>();
+            myList.Add("a");
+            myList.Add("b");
+            myList.Add("c");
+            myList.Add("d");
+            Console.WriteLine(myList.Count);
+        }
+    }
+        class MyList<T>
+        {
+            T[] array;
+            T[] temparray;
+
+            public MyList()
+            {
+                array = new T[0];
+            }
+
+            public void Add(T item)
+            {
+                temparray = array;
+                array = new T[array.Length + 1];
+                for (int i = 0; i < temparray.Length; i++)
+                {
+                array[i] = temparray[i];
+                }                                
+                array[array.Length - 1] = item;
+            }
+            
+            private int _count;
+
+            public int Count
+            {
+                get { return array.Length; }
+            }
+
+        }
+}
